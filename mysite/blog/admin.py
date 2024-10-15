09f9b1cc5ac239f2916e643c7d5af4f5f95682ef
tuchango <lapindanil22@gmail.com>
2024-10-15 23:18:52 +0300

    init commit

---
+++ mysite/blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Post
+
+
+# простейший вариант добавления таблицы в админ-панель
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'published', 'status']  # добавить столбцы таблицы
+    list_filter = ['published', 'author', 'status']  # справа появится меню с фильтрами
+    search_fields = ['title', 'body']  # поиск пройдет по этим полям
+    prepopulated_fields = {'slug': ('title',)}  # слаг заполняется автоматом по заголовку, при создании поста
+    date_hierarchy = 'published'  # можно над таблицей выбирать дату
+    raw_id_fields = ['author']  # по умолчанию автор выбирался из выпадашки, теперь через поле ввода, по id
+    show_facets = admin.ShowFacets.ALWAYS  # по умолчанию показывать кол-во постов для каждого фильтра
